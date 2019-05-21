7ad21ea73a7e1b82e02b3cb1604304fbaf76fb51
unknown <contactsachinl@gmail.com>
2019-05-21 09:55:00 -0700

    css change

--- src/components/CarouselModal.js
+++ src/components/CarouselModal.js
@@ -8,12 +8,12 @@ export const OpenModal = (props) => {
 
 	const customStyles = {
 		  content : {
-		    top                   : '50%',
-		    left                  : '50%',
-		    right                 : 'auto',
-		    bottom                : 'auto',
+		    top                   : "50%",
+		    left                  : "50%",
+		    right                 : "auto",
+		    bottom                : "auto",
 		    marginRight           : '-50%',
-		    transform             : 'translate(-50%, -50%)'
+		    transform             : "translate(-50%, -50%)"
 		  }
 	};
 
